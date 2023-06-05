de473d04368b069d0b6010e2a3c285bf36a8ce99
Sagar Bhadauriya <72752355+sagarbhadauriya@users.noreply.github.com>
2023-06-05 11:01:59 +0530

    Create dummyContacts.js

---
+++ app/dummyContacts.js
@@ -0,0 +1,174 @@
+const dummyContacts = [
+    {
+        firstName: 'Aarav',
+        lastName: 'Patel',
+        phoneNumbers: [
+        { number: '1234567890' },
+        ],
+        },
+        
+        {
+        firstName: 'Aanya',
+        lastName: 'Sharma',
+        phoneNumbers: [
+        { number: '9876543210' },
+        { number: '5555555555' },
+        ],
+        },
+        
+        {
+        firstName: 'Aryan',
+        lastName: 'Gupta',
+        phoneNumbers: [
+        { number: '9999999999' },
+        ],
+        },
+        
+        {
+        firstName: 'Ishaan',
+        lastName: 'Kumar',
+        phoneNumbers: [
+        { number: '1111111111' },
+        { number: '2222222222' },
+        ],
+        },
+        
+        {
+        firstName: 'Diya',
+        lastName: 'Chopra',
+        phoneNumbers: [
+        { number: '3333333333' },
+        ],
+        },
+        
+        {
+        firstName: 'Advait',
+        lastName: 'Joshi',
+        phoneNumbers: [
+        { number: '4444444444' },
+        { number: '6666666666' },
+        ],
+        },
+        
+        {
+        firstName: 'Anaya',
+        lastName: 'Malhotra',
+        phoneNumbers: [
+        { number: '7777777777' },
+        ],
+        },
+        
+        {
+        firstName: 'Arjun',
+        lastName: 'Shah',
+        phoneNumbers: [
+        { number: '8888888888' },
+        { number: '9999999999' },
+        ],
+        },
+        
+        {
+        firstName: 'Sara',
+        lastName: 'Verma',
+        phoneNumbers: [
+        { number: '1234567890' },
+        { number: '5555555555' },
+        ],
+        },
+        
+        {
+        firstName: 'Kabir',
+        lastName: 'Reddy',
+        phoneNumbers: [
+        { number: '1111111111' },
+        ],
+        },
+        
+        {
+        firstName: 'Aanya',
+        lastName: 'Nair',
+        phoneNumbers: [
+        { number: '2222222222' },
+        { number: '3333333333' },
+        ],
+        },
+        
+        {
+        firstName: 'Vivaan',
+        lastName: 'Pillai',
+        phoneNumbers: [
+        { number: '4444444444' },
+        { number: '5555555555' },
+        { number: '6666666666' },
+        ],
+        },
+        
+        {
+        firstName: 'Avni',
+        lastName: 'Raj',
+        phoneNumbers: [
+        { number: '7777777777' },
+        { number: '8888888888' },
+        ],
+        },
+        
+        {
+        firstName: 'Aarush',
+        lastName: 'Srinivasan',
+        phoneNumbers: [
+        { number: '9999999999' },
+        ],
+        },
+        
+        {
+        firstName: 'Myra',
+        lastName: 'Kulkarni',
+        phoneNumbers: [
+        { number: '1234567890' },
+        ],
+        },
+        
+        {
+        firstName: 'Reyansh',
+        lastName: 'Sharma',
+        phoneNumbers: [
+        { number: '9876543210' },
+        { number: '5555555555' },
+        ],
+        },
+        
+        {
+        firstName: 'Advika',
+        lastName: 'Rao',
+        phoneNumbers: [
+        { number: '9999999999' },
+        ],
+        },
+        
+        {
+        firstName: 'Dhruv',
+        lastName: 'Singh',
+        phoneNumbers: [
+        { number: '1111111111' },
+        { number: '2222222222' },
+        ],
+        },
+        
+        {
+        firstName: 'Neha',
+        lastName: 'Mishra',
+        phoneNumbers: [
+        { number: '3333333333' },
+        ],
+        },
+        {
+        firstName: 'Vihaan',
+        lastName: 'Desai',
+        phoneNumbers: [
+        { number: '4444444444' },
+        { number: '6666666666' },
+        ],
+        },
+  ];
+  
+  export default dummyContacts;
